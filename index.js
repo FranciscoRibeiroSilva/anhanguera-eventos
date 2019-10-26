9351c7e5014f450ad80432268c0a627ee7fd1e6e
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2019-10-25 22:19:14 -0200

    Adicionado modulo sequelize

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Teste: Okari na sai");
+    res.sendFile(__dirname + "/Management/index.html");
 });
+
+app.get ("/cast", function(req, res){
+    res.sendFile(__dirname + "/Management/LoginAdm.html");
+})
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na URL http://localhost:8081");
