1c5d2330aedf04f98034504c13335ee45ad110c0
MatheusHolanda07 <matheus.lopes8807@gmail.com>
2019-11-22 17:00:17 -0300

    FormParticipante linkado com user.js

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,28 @@ const CadastroUsers = require('../models/Usuarios')
 
 router.get('/geraCertificado', (req, res)=>{
     CadastroUsers.findAll().then(function(certis){
-    res.render('Certificado', {certise: certis})
+    res.render('user/Certificado', {certise: certis})
     })
 })
 
 router.get('/formPartic', (req, res)=>{
     res.render('user/userForms/FormParticipante')
 })
+
+//adiciona usuarios
+router.post('/addUsuario', (req, res)=>{
+    CadastroUsers.create({
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        email: req.body.email,
+        senha: req.body.senha
+    }).then(function(){
+        res.send('usuarios adicionado')
+    }).catch(function(err){
+        req.flash("error_msg", "Erro ao adicionar usuario")
+        //res.redirect('/anhangueraeventos/gerenciaDeAtividades')
+    })
+})
+
 
 module.exports = router
