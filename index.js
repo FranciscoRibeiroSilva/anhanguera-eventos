1cbed2be51f1f0cb4ad757a476ca31d21d0c163e
JohnOrtsac <jao_maratona_@outlook.com>
2019-12-03 03:29:11 -0300

    Edição das paginas

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Rotas
 //Rota principal
 app.get('/', function(req, res) {
-    res.render('admi/adminForms/FormAtividade')
+    res.render('user/loginPar')
 })
 
 //Rotas dos adm's
