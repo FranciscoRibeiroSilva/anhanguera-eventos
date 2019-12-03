d9cde564372b2c76692845edd43573217c894568
JohnOrtsac <jao_maratona_@outlook.com>
2019-12-03 00:49:40 -0300

    Alterando rodapé FormAdm

--- index.js
+++ index.js
@@ -11,49 +11,49 @@ const passaport = require("passport")
 require("./config/auth")(passaport)
 
 //Configurações
-    //Sessão
-        app.use(session({
-            secret: "qualquer",
-            resave: true,
-            saveUninitialized: true
-        }))
-        app.use(passaport.initialize())
-        app.use(passaport.session())
-        app.use(flash())
+//Sessão
+app.use(session({
+    secret: "qualquer",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(passaport.initialize())
+app.use(passaport.session())
+app.use(flash())
 
-    //Midleware
-        app.use((req, res, next)=>{
-            res.locals.success_msg = req.flash("success_msg")
-            res.locals.error_msg = req.flash("error_msg")
-            res.locals.error = req.flash("error")
-            next()
-        })
-        
-    //handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
+//Midleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flash("error_msg")
+    res.locals.error = req.flash("error")
+    next()
+})
 
-    //Body Parse
-        app.use(bodyParser.urlencoded({extended: false}))
-        app.use(bodyParser.json())
+//handlebars
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-    //Css 
-        app.use(express.static(path.join(__dirname, "public")));
+//Body Parse
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Css 
+app.use(express.static(path.join(__dirname, "public")));
 
 //Rotas
-    //Rota principal
-        app.get('/', function(req, res){
-            res.render('index')
-        })
-    
-    //Rotas dos adm's
-        app.use('/anhangueraeventos', adm)
-    
-    //Rotas do usuario
-        app.use('/users', user)
+//Rota principal
+app.get('/', function(req, res) {
+    res.render('admi/adminForms/FormAdm')
+})
+
+//Rotas dos adm's
+app.use('/anhangueraeventos', adm)
+
+//Rotas do usuario
+app.use('/users', user)
 
 //Outros
 const PORT = 8081
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Servidor Rodando na URL http://localhost:8081");
 });
