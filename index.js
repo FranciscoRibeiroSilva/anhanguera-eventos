8562b0de237c0092ee5f926995234fbd2ea5dea1
BrunoCruz95 <brunocruz012013@gmail.com>
2019-12-10 11:00:05 -0300

    Algumas alterações

--- index.js
+++ index.js
@@ -79,9 +79,9 @@ app.use('/anhangueraeventos', adm)
 app.use('/users', user)
 
 //Outros
-//const PORT = 8081
-//app.listen(PORT, () => {
-//    console.log("Servidor Rodando na URL http://localhost:8081");
-///});
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log("Servidor Rodando na URL http://localhost:8081");
+});
 
 app.listen(process.env.PORT || 3000)
