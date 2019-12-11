ad962d7ee228c810def886316158ea849e247734
JohnOrtsac <jao_maratona_@outlook.com>
2019-12-11 15:04:22 -0300

    retirando minha pasta node-modules

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Rotas
 //Rota principal
 app.get('/', function(req, res) {
-    res.render('admi/adminForms/FormAtividade')
+    res.render('index')
 })
 
 //Rotas dos adm's
