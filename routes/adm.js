4c2f544156803b0fdf54f4d72d505a6d261caa7e
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2019-12-12 23:48:51 -0200

    salvando geral

--- routes/adm.js
+++ routes/adm.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const Administrador = require('./models/Administrador')
+const Administrador = require('../models/Administrador')
 const Eventos = require('../models/Eventos')
 const Atividades = require('../models/Atividades')
 const Usuarios = require('../models/Usuarios')
