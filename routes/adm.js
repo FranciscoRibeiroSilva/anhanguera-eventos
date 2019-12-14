4d7abc9b917bddab2b5f6fae745253a4bc7bf532
MatheusHolanda07 <matheus.lopes8807@gmail.com>
2019-12-14 08:59:21 -0300

    adc local e data no FormEvento

--- routes/adm.js
+++ routes/adm.js
@@ -201,20 +201,23 @@ router.get('/formEvento', (req, res) => {
 //Adiciona dados do formulario eventos ao DB
 router.post('/addEvento', (req, res) => {
     var erros = []
-    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null || req.body.nome.length < 2) {
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null || req.body.nome.length < 2 || req.body.nome.length>15) {
         erros.push({ texto: "NOME DO EVENTO INVALIDO" })
     }
-    if (!req.body.quantSalas || typeof req.body.quantSalas == undefined || req.body.quantSalas == null) {
+    if (!req.body.quantSalas || typeof req.body.quantSalas == undefined || req.body.quantSalas == null || req.body.quantSalas < 0) {
         erros.push({ texto: "QUANTIDADE DE SALAS INVALIDO" })
     }
-    if (!req.body.nomeAdm || typeof req.body.nomeAdm == undefined || req.body.nomeAdm == null || req.body.nomeAdm.length < 2) {
+    if (!req.body.nomeAdm || typeof req.body.nomeAdm == undefined || req.body.nomeAdm == null || req.body.nomeAdm.length < 2 || req.body.nomeAdm.length>15) {
         erros.push({ texto: "NOME DE ADMINISTRADOR INVALIDO" })
     }
-    if (!req.body.emailAdm || typeof req.body.emailAdm == undefined || req.body.emailAdm == null || req.body.emailAdm.length < 12) {
+    if (!req.body.emailAdm || typeof req.body.emailAdm == undefined || req.body.emailAdm == null || req.body.emailAdm.length < 12 || req.body.emailAdm.length>30) {
         erros.push({ texto: "EMAIL INVALIDO" })
     }
     if (req.body.participanteEs == "Escolher op") {
         erros.push({ texto: "ESCOLHA O NUMERO DE PARTICIPANTES ESPERADOS" })
+    }
+    if (!req.body.local || typeof req.body.local == undefined || req.body.local == null || req.body.local.length < 5) {
+        erros.push({ texto: "NOME DE ADMINISTRADOR INVALIDO" })
     }
 
     if (req.body.tipoEvento == "Escolher op") {
@@ -240,6 +243,8 @@ router.post('/addEvento', (req, res) => {
             quantSalas: req.body.quantSalas,
             nomeAdm: req.body.nomeAdm,
             emailAdm: req.body.emailAdm,
+            local: req.body.local,
+            data: req.body.data,
             eventoTipo: req.body.eventoTipo,
             valorEvento: req.body.valorEvento
         }).then(function () {
